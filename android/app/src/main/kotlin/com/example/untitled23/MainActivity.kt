989ac5ebcea652b8abfb17dfbd725573019b614d
sinan <sinanalagoz74@gmail.com>
2022-03-05 15:30:32 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/untitled23/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.untitled23
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Anasayfa"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+
+            GestureDetector(
+                onTap: ()
+                {
+                  print('Dokunuldu');
+                },
+                child: Container(color: Colors.lightGreen, width: 200, height: 200,))
+          ],
+        ),
+      ),
+
+    );
+  }
+}
